5ac6ac876f8fe8a5e99c79a70fd0485fcac7f9bd
Danil26m <111749790+Danil26m@users.noreply.github.com>
2023-06-24 15:10:46 +0300

    m

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,32 @@
 import { Formik, Form, Field} from "formik"
+import * as yup  from 'yup';
 import { NavLink } from "react-router-dom";
 export default function Login() {
+    const validationSchema = yup.object().shape({
+        email: yup.string().required('Required').email('Invalid email'),
+        password: yup.string().required('Required').min(6, 'Too Short!').max(12, 'Too Long!')
+    })
     return (
         <section>
                <Formik
+               validationSchema={validationSchema}
        initialValues={{
-         signIn: {
             email: '',
             password: '',
-         },
        }}
        onSubmit={values => {
          console.log(values);
        }}
      >
+        {({ errors, touched }) => (
        <Form>
-         <Field name="signIn.email" type="email" />
-         <Field name="signIn.password" type="password" />
+         <Field name="email"  />
+         {errors.email && touched.email ? <div>{errors.email}</div> : null}
+         <Field name="password"   />
+         {errors.password && touched.password ? <div>{errors.password}</div> : null}
          <button type="submit">Login</button>
        </Form>
+        )}
      </Formik>
      <NavLink to='/register'><button type="button">Register</button></NavLink>
         </section>
